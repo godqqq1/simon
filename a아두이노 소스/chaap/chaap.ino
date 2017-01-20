c56a1ac605533997014c4bd3efafcfc06aafeff1
godqqq1 <godqqq1@nate.com>
2017-01-20 15:45:18 +0900

    Add files via upload

--- a아두이노 소스/chaap/chaap.ino
+++ a아두이노 소스/chaap/chaap.ino
@@ -6,40 +6,14 @@
  *
  * This software is released under a BSD license. See the attached LICENSE file for details.
  */
-#include <Wire.h>
-#include <Nanoshield_ADC.h>
-
-Nanoshield_ADC adc;
-int channel = 0;
-
 void setup()
 {
   Serial.begin(9600);
-  Serial.print("16-bit ADC Nanoshield Test - Read voltage (channel A");
-  Serial.print(channel);
-  Serial.println(")");
-  adc.begin();
 }
 
 void loop()
 {
-  int i = 0;
-  float res1=0;
-  float res2=0;
-  while(1){
-    i ++;
-  
-  
-  if(i > 100){
+    int t = analogRead(A0);
+    Serial.println(-125+(t*0.606));
     
-    Serial.print(res1/102);
-    Serial.println("%");
-    Serial.print(res2/104);
-    Serial.println("C");
-    break;
-  }
-  res1 = res1 +(((adc.readVoltage(0))*2.5)/10.0)*100;
-  res2 = res2 +(-20+(((adc.readVoltage(1))*2.5)/10.0)*100);
-  }
-  
 }
